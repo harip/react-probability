58925d16524bdb6e3fc619106d45eeb22430b98b
Haricharan Pulugurta <charanp@gmail.com>
2023-03-31 16:11:59 -0400

    Update coin-action.ts

--- src/store/coin-action.ts
+++ src/store/coin-action.ts
@@ -1,19 +1,26 @@
 export enum CoinActionTypesEnum {
-    SET_NUMBER_OF_TRIALS = 'SET_NUMBER_OF_TRIALS'
-}
-
-interface NumberOfTrailsAction {
-    type: CoinActionTypesEnum.SET_NUMBER_OF_TRIALS
-}
+    SET_NUMBER_OF_TRIALS = 'SET_NUMBER_OF_TRIALS',
+    SET_NUMBER_OF_FLIPS = 'SET_NUMBER_OF_FLIPS',
+} 
 
 export type CoinActionTypes =  {
     type: CoinActionTypesEnum.SET_NUMBER_OF_TRIALS
     data: number
-};
+} | {
+    type: CoinActionTypesEnum.SET_NUMBER_OF_FLIPS
+    data: number
+} ;
 
 export function setNumberOfTrails(numberOfTrails: number) {
     return {
         type: CoinActionTypesEnum.SET_NUMBER_OF_TRIALS,
         data: numberOfTrails
+    }
+}
+
+export function setNumberOfFlips(numberOfFlips: number) {
+    return {
+        type: CoinActionTypesEnum.SET_NUMBER_OF_FLIPS,
+        data: numberOfFlips
     }
 }
