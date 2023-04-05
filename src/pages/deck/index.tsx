eed8c73e3de36579d37439de2fb0b0fa7544a94e
Hari <charanp@gmail.com>
2023-04-04 22:17:30 -0400

    added a dummy deck page

--- src/pages/deck/index.tsx
+++ src/pages/deck/index.tsx
@@ -1,8 +1,21 @@
+import HeaderComponent from "@/components/header";
+import { HeaderComponentProps } from "@/lib/models/HeaderModel";
+import styles from './deck.module.css';
+
 const DeckComponent = () => {
+    function getHeaderData(): HeaderComponentProps {
+        return {
+            previousComponent: 'coin',
+            title: 'Deck Probability'
+        }
+    }
+
     return (
-        <div>
-            I am deck component
-        </div>
+        <>
+            <div className={styles.title}>
+                <HeaderComponent {...getHeaderData()}></HeaderComponent>
+            </div>
+        </>
     )
 }
 
